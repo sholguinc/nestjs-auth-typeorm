42a3a1288aeba84e2ae6cfafe98fdd41f42d2f95
Sergio Holguin <sholguinc@uni.pe>
2022-12-12 12:49:32 -0500

    base project

--- src/config.ts
+++ src/config.ts
@@ -5,6 +5,9 @@ export default registerAs('config', () => {
     database: {
       name: process.env.DATABASE_NAME,
       port: process.env.DATABASE_PORT,
+      password: process.env.DATABASE_PASSWORD,
+      user: process.env.DATABASE_USERNAME,
+      host: process.env.DATABASE_HOST,
     },
     apiKey: process.env.API_KEY,
   };
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,10 +1,29 @@
 import { Module, Global } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigType } from '@nestjs/config';
+import config from '../config';
 
 const API_KEY = '12345634';
 const API_KEY_PROD = 'PROD1212121SA';
 
 @Global()
 @Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>): any => {
+        const { user, name, port, password, host } = configService.database;
+        return {
+          type: 'postgres',
+          host,
+          port,
+          username: user,
+          password,
+          database: name,
+        };
+      },
+    }),
+  ],
   providers: [
     {
       provide: 'API_KEY',
